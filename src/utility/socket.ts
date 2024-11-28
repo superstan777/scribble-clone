799b754051ebf064acccfdf8c336537c48359896
superstan777 <viir4d@gmail.com>
2024-11-28 15:36:05 +0100

    url update

--- src/utility/socket.ts
+++ src/utility/socket.ts
@@ -1,10 +1,10 @@
 import { io, Socket } from "socket.io-client";
 
-const SERVER_URL = "http://localhost:3001";
+// const SERVER_URL = "http://localhost:3001";
+const SERVER_URL = "https://scribble-clone-backend-iw1g.onrender.com";
 
 export const socket: Socket = io(SERVER_URL);
 
 socket.on("connect", () => {
   console.log("Socket connected:", socket.id);
-  // socket.emit("ready", { playerId: socket.id });
 });
